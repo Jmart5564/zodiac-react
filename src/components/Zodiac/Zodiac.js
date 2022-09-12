ffe2d61b99327a493b29462d96750662bb704583
Jmart5564 <Jmart5564@gmail.com>
2022-09-12 15:38:16 -0700

    fighting the css demons

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -2,9 +2,9 @@ import './Zodiac.css';
 
 export default function Zodiac({ name, dates, symbol }) {
   return (
-    <div className="zodiac" style={{ top }}>
-      <img alt={name} src={`${process.env.PUBLIC_URL}/zodiacImages/${name}.webp`} />
-      <span className="name">{name}</span>
+    <div className="zodiac">
+      <img className="zodiac-img" alt={name} src={`${process.env.PUBLIC_URL}/zodiacImages/${name}.webp`} />
+      <span className="name">{name.charAt(0).toUpperCase() + name.slice(1)}</span>
       <span>{dates}</span>
       <span>{symbol}</span>
     </div>
